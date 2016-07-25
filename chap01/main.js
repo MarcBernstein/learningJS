5778d1dde30724098e9ecaf80fa2806e3c52f6ff
Marc Bernstein <marc.bernstein@hudl.com>
2016-07-25 12:17:00 -0700

    Chap 01

--- chap01/main.js
+++ chap01/main.js
@@ -4,8 +4,16 @@ $(document).ready(function() {
     paper.install(window);
     paper.setup(document.getElementById('mainCanvas'));
 
-    var c = Shape.Circle(200, 200, 50);
-    c.fillColor = 'tomato';
+    var tool = new Tool();
+    tool.onMouseDown = function(event) {
+        var c = Shape.Circle(200, 200, 80);
+        c.fillColor = 'black';
+        var text = new PointText(200, 200);
+        text.justification = 'center';
+        text.fillColor = 'white';
+        text.fontSize = 20;
+        text.content = 'hello world!';
+    };
 
     paper.view.draw();
 });
