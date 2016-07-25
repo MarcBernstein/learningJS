78a8cc65a0da3fcd4f5f93ae8d88c62c1825d391
Marc Bernstein <github@marcanderica.org>
2016-07-24 18:21:47 -0700

    Chap 1

---
+++ learn.js
@@ -0,0 +1,17 @@
+/*eslint no-console: ["error", { allow: ["log", "warn", "error"] }] */
+
+function main() {
+  console.log("Hello")
+}
+
+main()
+console.log("echo")
+// prints "echo" to the console
+/*
+In the previous line, everything up to the double forward slashes
+is JavaScript code, and must be valid syntax. The double
+forward slashes start a comment, and will be ignored by JavaScript.
+This text is in a block comment, and will also be ignored
+by JavaScript. We've chosen to indent the comments of this block
+for readability, but that's not necessary.
+*/
